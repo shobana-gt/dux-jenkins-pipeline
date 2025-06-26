fbe2c66a02f16f7f2ac38fb5a4ae3d7d0492ee5a
Shobana Nagarajan Bagavathy <bshobana@omnissa.com>
2025-06-26 12:21:22 +0530

    Updated tunnel-debug for 3.0

--- tunnel-debug.groovy
+++ tunnel-debug.groovy
@@ -1,17 +1,47 @@
+def DUX_MAJOR_VERSION = 0
 pipeline {
     agent any
     stages {
+        stage('Set Dux Major Version') {
+            steps {
+                script {
+                    def duxVersion = sh(script: "dux version | tail -n 1", returnStdout: true).trim()
+                    echo "Dux version detected: ${duxVersion}"
+
+                    // Extract the first digit of the version
+                    DUX_MAJOR_VERSION = duxVersion.tokenize('.')[0]
+                    echo "Dux major version set to: ${DUX_MAJOR_VERSION}"
+                }
+            }
+        }
         stage('Run Dux Logs') {
             steps {
                 script {
                     echo "Fetching tunnel server logs for troubleshooting..."
+                    def command = "" // Define the command variable outside the try block
+
                     try {
-                        // Execute the `dux logs` command
-                        def logsOutput = sh(script: "dux logs", returnStdout: true).trim()
-                        echo "Dux Logs Output:\n${logsOutput}"
+                   /* echo "Checking Dux version..."
+                    def duxVersion = sh(script: "dux version | tail -n 1", returnStdout: true).trim()
+                    echo "Dux version detected: ${duxVersion}"
+
+                    // Extract the first digit of the version
+                    def majorVersion = duxVersion.tokenize('.')[0] as int*/
+
+                    if (DUX_MAJOR_VERSION >= 3) {
+                        echo "Dux version is 3 or higher. Running 'dux tunnel logs'..."
+                        command = params.HOST_IP == 'All' ? 'dux tunnel logs' : "dux tunnel logs -p ${params.HOST_IP}"
+                    } else {
+                        echo "Dux version is less than 3. Running 'dux logs'..."
+                        command = params.HOST_IP == 'All' ? 'dux tunnel logs' : "dux tunnel logs -p ${params.HOST_IP}"
+                    }
+
+                    // Execute the `dux logs` command
+                    def logsOutput = sh(script: command, returnStdout: true).trim()
+                    echo "Dux logs command output:\n${logsOutput}"
                     } catch (Exception e) {
                         // Handle errors if the `dux logs` command fails
-                        error "Failed to execute 'dux logs'. Error: ${e.message}"
+                        error "Failed to execute '${command}'. Error: ${e.message}"
                     }
                 }
             }
@@ -20,9 +50,17 @@ pipeline {
             steps {
                 script {
                     echo "Generating output of vpnreport command..."
+                    def command = "" 
+                    if (DUX_MAJOR_VERSION >= 3) {
+                        echo "Dux version is 3 or higher. Running 'dux tunnel report'..."
+                        command = params.HOST_IP == 'All' ? 'dux tunnel report' : "dux tunnel report -p ${params.HOST_IP}"
+                    } else {
+                        echo "Dux version is less than 3. Running 'dux report'..."
+                        command = params.HOST_IP == 'All' ? 'dux tunnel report' : "dux tunnel report -p ${params.HOST_IP}"
+                    }
                     try {
                         // Execute the `dux report` command
-                        def reportOutput = sh(script: "dux report", returnStdout: true).trim()
+                        def reportOutput = sh(script: command, returnStdout: true).trim()
                         echo "Dux Report Output:\n${reportOutput}"
                     } catch (Exception e) {
                         // Handle errors if the `dux report` command fails
