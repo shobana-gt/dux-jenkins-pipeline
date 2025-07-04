fc7c6861b448229c821d0ef78a2216b2f5244ec0
Shobana Nagarajan Bagavathy <bshobana@omnissa.com>
2025-07-04 18:27:34 +0530

    Added pacreader jobs -debug, healthcheck and container operations

---
+++ pacreader-restart.groovy
@@ -0,0 +1,117 @@
+node {
+    // Prepare the workspace and copy the manifest file
+    sh "cp /opt/omnissa/dux/pr_manifest.yml ${env.WORKSPACE}/pr_manifest.yml"
+}
+pipeline {
+    agent any
+    parameters {
+        choice(
+            name: 'HOST_IP',
+            choices: getHostIPs(),
+            description: 'Select the IP address of the host (or "All" for all hosts)'
+        )
+    }
+    stages {
+        stage('Set Dux Major Version') {
+            steps {
+                script {
+                    def duxVersion = sh(script: "dux version | tail -n 1", returnStdout: true).trim()
+                    echo "Dux version detected: ${duxVersion}"
+
+                    // Extract the first digit of the version
+                    env.DUX_MAJOR_VERSION = duxVersion.tokenize('.')[0]
+                    echo "Dux major version set to: ${env.DUX_MAJOR_VERSION}"
+                }
+            }
+        }
+        stage('Restart Container') {
+            steps {
+                script {
+                    echo "Restarting container deployed..."
+                    def command = ""
+
+                    if (env.DUX_MAJOR_VERSION.toInteger() >= 3) {
+                        echo "Dux version is 3 or higher. Running 'dux pacreader restart'..."
+                        command = params.HOST_IP == 'All' ? 'dux pacreader restart -y' : "dux pacreader restart -y -p ${params.HOST_IP}"
+                    } else {
+                        error "Dux version is less than 3.0 . pacreader container is not supported."
+                    }
+                    try {
+                        // Execute the `dux restart` command
+                        def restartOutput = sh(script: "${command} 2>&1", returnStdout: true).trim()
+                        echo "dux restart command output:\n${restartOutput}"
+                    } catch (Exception e) {
+                        // Handle errors if the `dux restart` command fails
+                        error "Failed to execute '${command}'. Error: ${e.message}"
+                    }
+                }
+            }
+        }
+        stage('Check Dux Status') {
+            steps {
+                script {
+                    echo "Checking Dux Status..."
+                   def command = "" 
+
+                    if (env.DUX_MAJOR_VERSION.toInteger() >= 3) {
+                        echo "Dux version is 3 or higher. Running 'dux pacreader status'..."
+                        command = params.HOST_IP == 'All' ? 'dux pacreader status' : "dux pacreader status -p ${params.HOST_IP}"
+                    } else {
+                        error "Dux version is less than 3.0 . PAC Reader container is not supported."
+                    }
+
+                    
+                    try {
+                        // Execute the `dux status` command
+                        def statusOutput = sh(script: command, returnStdout: true).trim()
+                        echo "dux status command output:\n${statusOutput}"
+                    } catch (Exception e) {
+                        // Handle errors if the `dux status` command fails
+                        error "Failed to execute '$command'. Error: ${e.message}"
+                    }
+                }
+            }
+        }
+    }
+    post {
+        success {
+            echo "Restart Container operation completed successfully."
+        }
+        failure {
+            echo "Restart Container operation failed."
+        }
+    }
+}
+
+def getHostIPs() {
+    def ips = ['All']
+
+    node {
+        try {
+            def manifestPath = "${env.WORKSPACE}/pr_manifest.yml"
+
+            def manifestContent = readFile(manifestPath)
+            echo "Manifest Content:\n${manifestContent}"
+
+            def yaml = new org.yaml.snakeyaml.Yaml()
+            def manifest = yaml.load(manifestContent)
+
+            if (manifest.pac_reader?.hosts) {
+                manifest.pac_reader.hosts.each { host ->
+                    if (host.address) {
+                        ips << host.address
+                    }
+                }
+            } else {
+                echo "No hosts found in the manifest file."
+            }
+
+            echo "Extracted IPs: ${ips}"
+        } catch (Exception e) {
+            echo "Failed to read or parse the manifest file: ${e.message}"
+        }
+    }
+
+    return ips.join('\n')
+}
+
